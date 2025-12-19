aa3152799817ea2084338fa399a5b6041d5e6a34
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-12-19 16:17:28 -0300

    add buttons with 3 variants to save, confirm changes and cancel changes

--- galponvial-front/src/App.tsx
+++ galponvial-front/src/App.tsx
@@ -4,16 +4,13 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col">
-      
       <Navbar />
 
       <main className="flex-1 p-8">
         <h1 className="text-3xl text-gray-800">Contenido de la página</h1>
-        <p className="text-gray-600 mt-4">
-          Aquí va el resto de la aplicación. Como ves, el navbar ya no ocupa todo.
-        </p>
+        <p className="text-gray-600 mt-4">Contenido futuro del sitio</p>
       </main>
-      
+
     </div>
   );
 }
---
+++ galponvial-front/src/components/Button.tsx
@@ -0,0 +1,31 @@
+type ButtonVariant = "save" | "changes" | "danger";
+
+interface ButtonProps {
+  children: React.ReactNode;
+  variant?: ButtonVariant;
+  onClick?: () => void;
+}
+
+const Button = ({ children, variant = "save", onClick }: ButtonProps) => {
+  const base =
+    "inline-flex items-center justify-center font-semibold rounded-md border-3 transition-colors duration-200 px-4 py-2";
+
+  const variants = {
+    save:
+      "max-w-[100px] w-full bg-blue-600 text-white border-blue-700 hover:bg-blue-500 hover:border-blue-500",
+
+    changes:
+      "max-w-[180px] w-full bg-blue-600 text-white border-blue-700 hover:bg-blue-500 hover:border-blue-500",
+
+    danger:
+      "max-w-[100px] w-full bg-white text-red-600 border-red-600 hover:bg-red-600 hover:text-white hover:border-red-700",
+  };
+
+  return (
+    <button onClick={onClick} className={`${base} ${variants[variant]}`}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
