33ee6a1e14dcd31fbfb4585cb98d5db6c472cf5e
Manuel <mrmanuelalvarez@gmail.com>
2025-12-22 22:46:40 -0300

    arreglados estilos en linea y otros problemas generales

--- galponvial-front/src/App.tsx
+++ galponvial-front/src/App.tsx
@@ -1,18 +1,18 @@
-import "./App.css";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col">
+    <div className="min-h-screen flex flex-col">
       
       <Navbar />
 
-      <main className="flex-1 p-8">
+      <div className="flex-1 p-8">
         <h1 className="text-3xl text-gray-800">Contenido de la página</h1>
-        <p className="text-gray-600 mt-4">
+        <p className="text-red-600 mt-4">
           Aquí va el resto de la aplicación. Como ves, el navbar ya no ocupa todo.
         </p>
-      </main>
+        <p className="text-blue-600">Hola</p>
+      </div>
       
     </div>
   );
--- galponvial-front/src/shared/ui/Badge.tsx
+++ galponvial-front/src/shared/ui/Badge.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { type ReactNode } from 'react';
 
 interface BadgeProps {
   variant?: 'primary' | 'success' | 'warning' | 'danger' | 'info';
